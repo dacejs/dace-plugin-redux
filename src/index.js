54184890430ce6a110048ad398d9858f9be7bb42
zhongzhi107 <zhong.zhi@163.com>
2019-01-11 10:32:21 +0800

    #5 对外暴露 axios 实例

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
-export { default as getInitialProps } from './getInitialProps';
 export { default } from './plugin';
+export { default as getInitialProps } from './getInitialProps';
+export { default as axios } from './axiosInstance';
