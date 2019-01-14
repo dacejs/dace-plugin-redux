8620e5fbfe10df93140f7a721d78e8fbc473e67b
zhongzhi107 <zhong.zhi@163.com>
2019-01-14 18:43:27 +0800

    feat: 增加 `DACE_AXIOS_INSTANCE_PATH` 参数
    创建 axios 实例的文件路径

--- src/createStore.js
+++ src/createStore.js
@@ -2,7 +2,8 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import mergeable from 'redux-merge-reducers';
-import axiosInstance from './axiosInstance';
+import axios from 'axios';
+
 /**
  * 创建 store
  *
@@ -12,7 +13,39 @@ export default (req) => {
   const isClient = typeof window === 'object';
   const initialState = isClient ? window.INITIAL_STATE : {};
 
-  const middlewares = [thunk.withExtraArgument(axiosInstance(req))]
+  let axiosInstance;
+
+  try {
+    // 如果传入已有的 axios 实例，修改之
+    axiosInstance = require(process.env.DACE_AXIOS_INSTANCE_PATH);
+    if (axiosInstance.default) {
+      axiosInstance = axiosInstance.default;
+    }
+  } catch (e) {
+    // 否则新建一个 axios 实例
+    axiosInstance = axios.create();
+  }
+
+  let baseURL = process.env.DACE_API_BASE_URL;
+  // 不传 DACE_API_BASE_URL 时使用当前域名
+  if (req && !baseURL) {
+    baseURL = `${req.protocol}://${req.headers.host}`;
+  }
+
+  let headers = {};
+  if (!isClient) {
+    // 服务器端请求 API 时，透传原始请求的 headers
+    headers = {
+      ...req.headers,
+      'X-Real-IP': (req.ip || '').split(',')[0]
+    };
+  }
+
+  axiosInstance.defaults.baseURL = baseURL;
+  axiosInstance.defaults.headers = headers; // 透穿headers
+  axiosInstance.defaults.withCredentials = true; // 允许携带cookie
+
+  const middlewares = [thunk.withExtraArgument(axiosInstance)]
     .concat(require('./reduxMiddlewares'));
 
   // 初始化使用的默认 reducer
--- test/dace.config.js
+++ test/dace.config.js
@@ -1,5 +1,11 @@
 module.exports = {
+  // 创建 axios 实例的文件路径
+  DACE_AXIOS_INSTANCE_PATH: 'src/axios.js',
+
+  // 加上 dace-plugin-redux
   plugins: ['redux'],
+
+  // 修改 webpack 配置
   modify(config, { target }) {
     const appConfig = config;
     if (target === 'web') {
