1abf9b7779e6a4cb1971d4d0b991c6af2069db64
zhongzhi107 <zhong.zhi@163.com>
2019-01-28 19:57:01 +0800

    Update createStore.js

--- src/runtime/createStore.js
+++ src/runtime/createStore.js
@@ -13,23 +13,13 @@ export default (req) => {
   const initialState = isClient ? window.INITIAL_STATE : {};
   const axiosInstance = require(process.env.DACE_PATH_AXIOS_INSTANCE);
 
-  // let baseURL = process.env.DACE_API_BASE_URL;
-  // 不传 DACE_API_BASE_URL 时使用当前域名
   if (req) {
-    baseURL = `${req.protocol}://${req.headers.host}`;
-  }
-
-  let headers = {};
-  if (!isClient) {
-    // 服务器端请求 API 时，透传原始请求的 headers
-    headers = {
+    axiosInstance.defaults.baseURL = `${req.protocol}://${req.headers.host}`;
+    axiosInstance.defaults.headers = {
       ...req.headers,
       'X-Real-IP': (req.ip || '').split(',')[0]
-    };
+    }; // 透穿headers
   }
-
-  axiosInstance.defaults.baseURL = baseURL;
-  axiosInstance.defaults.headers = headers; // 透穿headers
   axiosInstance.defaults.withCredentials = true; // 允许携带cookie
 
   const middlewares = [thunk.withExtraArgument(axiosInstance)]
